52f64fefda55edc83df8a6beb745ac2b23b68e5f
Wicho <lgamez@Wichos-Mac-mini.local>
2021-06-30 09:49:31 -0600

    Fist commit

---
+++ my_app/__init__.py
@@ -0,0 +1,6 @@
+import time
+
+
+def add(arg):
+    time.sleep(10)
+    return sum(arg)
---
+++ tests/test_add_unittest.py
@@ -0,0 +1,19 @@
+import unittest
+
+from my_app import add
+from unittest.mock import patch
+
+
+class TestAdd(unittest.TestCase):
+    @patch('my_app.add', return_value=6)
+    def test_add_integer_list(self, add):
+        """
+        Probar que la función suma números enteros
+        """
+        data = [1, 2, 3]
+        result = add(data)
+        self.assertEqual(result, 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
